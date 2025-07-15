a288c47aeffe188e19a2b4973367adb28a63f204
Tim Fanselow <tim.fanselow@gmail.com>
2025-07-14 19:36:38 -0700

    Implement main.cpp - serial interface to control robot

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,64 @@
-#include "Arduino.h"
-#include <Adafruit_PWMServoDriver.h>
+// main.cpp
+// Exposes a serial interface to control the robot.
+//  Simply takes strings over serial of form "<forward_val>,<turn_val>\n"
+// where forward_val and turn_val specify signed floats.
+// A bit inefficient to encode via strings, but quick and easy.
 
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-void setup()
-{
-  // initialize LED digital pin as an output.
+#include <Arduino.h>
+#include <Adafruit_I2CDevice.h>
+#include <Motor.h>
+#include <DriveSystem.h>
+#include <cmath>
+
+#define DEG2RAD M_PI/180.f
+#define HWSERIAL Serial1  // rx pin 0, tx pin 1
+
+#define SERIAL_TIMEOUT 1000  // stop robot if no serial received in this many milliseconds 
+
+unsigned long last_receive_time;
+
+Adafruit_PWMServoDriver pwm_driver = Adafruit_PWMServoDriver();
+
+DriveSystem drive_system(
+    Motor(pwm_driver, 1, 0), // right rear
+    Motor(pwm_driver, 2, 3), // right front
+    Motor(pwm_driver, 5, 4), // left front
+    Motor(pwm_driver, 7, 6) // left rear
+);
+
+void setup() {
+  Serial.begin(9600); // debug port
+  HWSERIAL.begin(9600);
+  setup_pwm_driver(pwm_driver);
   pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(18, INPUT_PULLUP);
-  pinMode(19, INPUT_PULLUP);
-
-  pwm.begin();
-  pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(50); 
-
-  Serial.begin(9600);
-
-  
+  drive_system.set_speed(0,0);
+  Serial.println("\n Exposing serial interfact to control robot");
 }
 
-int loop_count = 0;
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+void loop() {
 
-  loop_count++;
-  Serial.print("loop ");
-  Serial.println(loop_count);
+  if (HWSERIAL.available()) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    String command = HWSERIAL.readStringUntil('\n'); // Read until newline
+    command.trim();
+    Serial.println(String("Received command: ") + command);
+    int comma_index = command.indexOf(',');
+    if (comma_index != -1) {
+      String forwards_str = command.substring(0, comma_index);
+      String turn_str = command.substring(comma_index + 1);
 
-  Serial.print("readPrescale: ");
-  Serial.println(pwm.readPrescale());
-  for (uint8_t pwmnum=0; pwmnum < 2; pwmnum++) {
-    // pwm.setPWM(pwmnum, 0, 4096 );
-    pwm.setPWM(pwmnum, 4096, 0 );
-    delay(100);
+      float forwards_value = forwards_str.toFloat(); 
+      float turn_value = turn_str.toFloat();
+
+      Serial.print("Received: Forwards = " + String(forwards_value, 2) + ", Turn = " + String(turn_value, 2) + "\n");
+      drive_system.set_speed(forwards_value, turn_value);
+      last_receive_time = millis();
+    }
+  } else if ((millis() - last_receive_time) > SERIAL_TIMEOUT){
+    drive_system.set_speed(0,0);
+    Serial.println("Timeout receiving command, set speed to 0");
+    last_receive_time = millis();
   }
-
-  // wait for a second
-  delay(1000);
-
   digitalWrite(LED_BUILTIN, LOW);
-
-  for (uint8_t pwmnum=0; pwmnum < 2; pwmnum++) {
-    pwm.setPWM(pwmnum, 0, 4096 );
-  }
-
-   // wait for a second
-  delay(1000);
 }
-
-
-
-// #include <Arduino.h>
-// #include <Wire.h> // Include the I2C library
-
-// void setup() {
-//   Serial.begin(9600); // Start serial communication
-//   while (!Serial && millis() < 5000); // Wait for serial port to connect. Needed for native USB port only.
-//   Serial.println("\nI2C Scanner");
-
-//   // Initialize the I2C bus
-//   pinMode(18, INPUT_PULLUP);
-//   pinMode(19, INPUT_PULLUP);
-//   Wire.begin();
-//   Wire.setClock( 10000UL);
-// }
-
-// void loop() {
-//   byte error, address;
-//   int nDevices;
-
-//   Serial.println("Scanning...");
-
-//   nDevices = 0;
-//   for (address = 1; address < 127; address++) {
-//     // The i2c_scanner uses the return value of
-//     // the Wire.endTransmission to see if
-//     // a device did acknowledge to the address.
-//     Wire.beginTransmission(address);
-//     error = Wire.endTransmission();
-
-//     if (error == 0) {
-//       Serial.print("I2C device found at address 0x");
-//       if (address < 16) {
-//         Serial.print("0");
-//       }
-//       Serial.print(address, HEX);
-//       Serial.println("  !");
-//       nDevices++;
-//     } else {
-//       Serial.print("Error ");
-//       Serial.print(error);
-//       Serial.print(" at address 0x");
-//       if (address < 16) {
-//         Serial.print("0");
-//       }
-//       Serial.println(address, HEX);
-//     }
-//   }
-//   if (nDevices == 0) {
-//     Serial.println("No I2C devices found\n");
-//   } else {
-//     Serial.println("done\n");
-//   }
-
-//   delay(5000); // Wait 5 seconds for next scan
-// }
